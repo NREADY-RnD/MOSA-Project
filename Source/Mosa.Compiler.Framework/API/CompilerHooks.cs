0081301e219fbefc29aff0da7ad735f3eaa2b4c1
Phil Garcia <phil@thinkedge.com>
2019-12-07 09:05:54 -0800

    - WIP

---
+++ Source/Mosa.Compiler.Framework/API/CompilerHooks.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mosa.Compiler.Framework.API
+{
+	public class CompilerHooks
+	{
+		public
+	}
+}
